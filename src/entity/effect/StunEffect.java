f3a74b621ab7438d40524e8a28060358e2baa8b7
unknown <RAHINI001@e.ntu.edu.sg>
2026-04-15 18:22:11 +0530

    stuneffect changes

--- src/entity/effect/StunEffect.java
+++ src/entity/effect/StunEffect.java
@@ -1,34 +1,27 @@
 package entity.effect;
-
 import entity.combatant.Combatant;
-
 /**
- * Smoke Bomb effect: enemy attacks deal 0 damage.
- * Duration: 2 turns (current turn + next turn).
- */
-public class SmokeBombEffect extends StatusEffect {
-
-  public SmokeBombEffect() {
-    super("Smoke Bomb Invulnerability", 2);
-  }
-
-  @Override
-  public void onApply(Combatant target) {
-    // No stat change, checked dynamically
-  }
-
-  @Override
-  public void onTurnStart(Combatant target) {
-    // No per-turn effect
-  }
-
-  @Override
-  public void onRemove(Combatant target) {
-    // No cleanup
-  }
-
-  @Override
-  public boolean negatesDamage() {
-    return true;
-  }
+* Stun effect: affected entity cannot take actions.
+* Duration: 2 turns (current turn + next turn).
+*/
+public class StunEffect extends StatusEffect {
+public StunEffect(int duration) {
+super("Stun", duration);
+}
+@Override
+public void onApply(Combatant target) {
+// No stat change on application
+}
+@Override
+public void onTurnStart(Combatant target) {
+// Stun just prevents action, no additional effect
+}
+@Override
+public void onRemove(Combatant target) {
+// No cleanup needed
+}
+@Override
+public boolean preventsAction() {
+return true;
+}
 }
